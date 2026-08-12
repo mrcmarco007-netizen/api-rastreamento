315d2046bb676ecfeb62dee4e4ebb89776da7895
Marco <mrcmarco007@gmail.com>
2026-08-11 23:49:14 -0400

    corrige senha2

--- backend/database/database.py
+++ backend/database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Clash051201%40230520@db.ehsrheoftxuxelxprjye.supabase.co:5432/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Clash051201%40%230520@db.ehsrheoftxuxelxprjye.supabase.co:5432/postgres"
 
 if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
